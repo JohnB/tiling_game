d00d86c88e7a61469a0719dd110b4bb219bf5400
JohnB <john.baylor@gmail.com>
2019-08-23 21:12:24 -0700

    Add exdoc. Generate with "mix docs".

--- mix.exs
+++ mix.exs
@@ -2,12 +2,24 @@ defmodule TilingGame.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :tiling_game,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :tiling_game,
+      version: "0.1.0",
+      elixir: "~> 1.8",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      
+      # Docs
+      name: "Tiling Game",
+      source_url: "https://github.com/JohnB/tiling_game",
+      homepage_url: "https://tilinggame.com",
+      docs: [
+        main: "TilingGame",
+        #logo: "path/to/logo.png",
+        extras: ["README.md"]
+      ]
+    ]
   end
 
   # Configuration for the OTP application
@@ -29,6 +41,8 @@ defmodule TilingGame.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
   end
 end
